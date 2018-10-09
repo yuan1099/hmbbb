53a02a3616d48f214ecdb9e7dc654c7a11d4af96
yuan1099 <1099632887@qq.com>
2018-10-09 19:14:01 +0800

    首首页创建基础完成

--- src/main.js
+++ src/main.js
@@ -1,8 +1,30 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+// 引入路由
+import VueRouter from 'vue-router';
+
+    Vue.use(VueRouter);
+// 引入组件
+import index from './components/index.vue';
+
+
+const routes=[
+  {
+    path: '/',
+    component: index
+  },
+  {
+  path: '/index',
+  component: index
+}]
+const router = new VueRouter({
+  routes // (缩写) 相当于 routes: routes
+})
+
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App)
+  render: h => h(App),
+  router
 }).$mount('#app')
